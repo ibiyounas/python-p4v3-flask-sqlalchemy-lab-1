b3496331f6147cb953ef2b30fb26768c4c76a6ee
Ibrahem Younas <younasibrahem@gmail.com>
2024-06-19 00:32:37 -0400

    passes all tests

--- server/app.py
+++ server/app.py
@@ -21,7 +21,34 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def get_by_id(id):
+    earthquake = Earthquake.query.filter(Earthquake.id == id).first()
+    if earthquake:
+        body = earthquake.to_dict()
+        status = 200
+    else:
+        body = {'message': f'Earthquake {id} not found.'}
+        status = 404
+    return make_response(body, status)
 
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def get_by_min_mag(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    if earthquakes:
+        quakes = [earthquake.to_dict() for earthquake in earthquakes]
+        body = {
+            'count': len(quakes),
+            'quakes': quakes
+        }
+        status = 200
+    else:
+        body = {
+            'count': 0,
+            'quakes': []
+        }
+        status = 200
+    return make_response(body, status)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
